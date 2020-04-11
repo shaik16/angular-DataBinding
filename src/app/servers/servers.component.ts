10ff0ad775dabeb9f0a3cdd662b21218e135b01d
shaik16 <sadath.sa@gmail.com>
2020-04-11 13:36:42 +0530

    1 file modified, 1 added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  templateUrl: './servers.component.html'
 })
 export class ServersComponent{
   allowNewServer:boolean=false;
