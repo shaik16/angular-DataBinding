c3d08575338ac983d4504e3a292175761fcbb695
shaik16 <sadath.sa@gmail.com>
2020-04-12 16:02:04 +0530

    4 files modified

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles:[`
+      .online{
+        color: white;
+      }
+    `]
 })
 
 export class ServerComponent{
     id:number=2
     status:string='online'
+
+    constructor(){
+      this.status=Math.random() > 0.5 ? 'online' : 'offline'
+    }
+
+    getColor(){
+      return this.status=== 'online' ? 'green' : 'red'
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,16 +9,19 @@ export class ServersComponent{
   serverCreationStatus='No server was created';
   serverName='Test';
   serve="Add Server";
+  servers=['Testserver']
   constructor(){
     setTimeout(() => {
       this.allowNewServer=true;
     }, 3000);
   }
   onCreateServer(){
+    this.servers.push(this.serverName)
     this.serverCreationStatus='Server was created! and Name is '+this.serverName;
     this.serve='Delete Server'
   }
   onDeleteServer(){
+    this.servers.pop()
     this.serverCreationStatus='Server was Deleted! and Name is '+this.serverName;
     this.serve='Add Server'
   }
